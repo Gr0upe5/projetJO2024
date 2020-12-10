ea20a3eda5d7afbc6d78f8f2144c6c1b6e6ca92d
adelidev <adel.terki2@outlook.fr>
2020-12-10 19:16:31 +0100

    +

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,444 @@
-$(document).ready(function() {
+//INITIALISATION DE LA MAP
+const mapCenter = { lat: 48.8534, lng: 2.20 };
+const map = new google.maps.Map(document.getElementById('map'), {
+    center: mapCenter,
+    zoom: 10,
+    disableDefaultUI: true,
+    mapTypeId: 'satellite',
+});
+
+//TABLEAU DES MARKERS
+var markers = new Array(
+    //Champs-Élysées
+    new google.maps.Marker({
+        position: { lat: 48.869745, lng: 2.307946 },
+        map,
+        title: 'Champs-Élysées',
+        visible: false,
+        cyclisme: true,
+        paris: true,
+    }),
+    //Champs-de-Mars
+    new google.maps.Marker({
+        position: { lat: 20, lng: 20 },
+        map,
+        title: 'Champs-de-Mars',
+        visible: false,
+        beachv: true,
+        judo: true,
+        lutte: true,
+        paris: true,
+    }),
+    //Tour Eiffel
+    new google.maps.Marker({
+        position: { lat: 48.858370, lng: 2.294481 },
+        title: 'Tour Eiffel',
+        map,
+        visible: false,
+        triathlon: true,
+        natation: true,
+        paris: true,
+    }),
+    //Stade de France
+    new google.maps.Marker({
+        position: { lat: 48.924459, lng: 2.360164 },
+        map,
+        title: 'Stade de France',
+        visible: false,
+        athletisme: true,
+        stdenis: true,
+    }),
+    //Village olympique de Saint-Denis
+    new google.maps.Marker({
+        position: { lat: 48.8665, lng: 2.3373 },
+        title: 'Village olympique de Saint-Denis',
+        map,
+        visible: false,
+        stdenis: true,
+    }),
+    //Esplanade des Invalides
+    new google.maps.Marker({
+        position: { lat: 48.8565, lng: 2.3131 },
+        map,
+        title: 'Esplanade des Invalides',
+        visible: false,
+        tir: true,
+        paris: true,
+    }),
+    //Stade Jean-Bouin
+    new google.maps.Marker({
+        position: { lat: 48.8398, lng: 2.2529 },
+        map,
+        title: 'Stade Jean-Bouin',
+        visible: false,
+        paris: true,
+    }),
+    //Stade olympique Yves-du-Manoir
+    new google.maps.Marker({
+        position: { lat: 48.9290, lng: 2.2479 },
+        map,
+        title: 'Stade olympique Yves-du-Manoir',
+        visible: false,
+        yvesmanoir: true,
+    }),
+    //Zénith de Paris
+    new google.maps.Marker({
+        position: { lat: 48.8907, lng: 2.3933 },
+        map,
+        title: 'Zénith de Paris',
+        visible: false,
+        halthero: true,
+        paris: true,
+    }),
+    //Base nautique de Vaires-sur-Marne
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: 'Base nautique de Vaires-sur-Marne',
+        visible: false,
+        canoekayak: true,
+        aviron: true,
+    }),
+    //Château de Versailles
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: 'Château de Versailles',
+        visible: false,
+        equitation: true,
+        pentathlon: true,
+        versailles: true,
+        stquentin: true,
+    }),
+    //Vélodrome national
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: 'Vélodrome national',
+        visible: false,
+        cyclisme: true,
+        pentathlon: true,
+    }),
+    //Grande halle de la Villette
+    new google.maps.Marker({
+        position: { lat: 48.8873, lng: 2.3909 },
+        map,
+        title: 'Grande halle de la Villette',
+        visible: false,
+    }),
+    //Stade Pierre-de-Coubertin
+    new google.maps.Marker({
+        position: { lat: 48.8310, lng: 2.2525 },
+        map,
+        title: 'Stade Pierre-de-Coubertin',
+        visible: false,
+        basketball: true,
+        paris: true,
+    }),
+    //Vélodrome de Saint-Quentin-en-Yveliness
+    new google.maps.Marker({
+        position: { lat: 48.7882, lng: 2.0347 },
+        map,
+        title: 'Vélodrome de Saint-Quentin-en-Yvelines',
+        visible: false,
+        hockey: true,
+        stquentin: true,
+    }),
+    //Colline d'Élancourt
+    new google.maps.Marker({
+        position: { lat: 48.7878, lng: 1.9679 },
+        map,
+        title: "Colline d'Élancourt",
+        visible: false,
+        cyclisme: true,
+        elancourt: true,
+    }),
+    //Golf national
+    new google.maps.Marker({
+        position: { lat: 48.7468, lng: 2.0715 },
+        map,
+        title: "Golf national",
+        visible: false,
+        golf: true,
+        stquentin: true,
+    }),
+    //Parc des expositions du Bourget
+    new google.maps.Marker({
+        position: { lat: 48.9356, lng: 2.4321 },
+        map,
+        title: "Parc des expositions du Bourget",
+        visible: false,
+        volleyb: true,
+        lebourget: true,
+    }),
+    //Stand de tir du Bourget
+    new google.maps.Marker({
+        position: { lat: 48.9356, lng: 2.4321 },
+        map,
+        title: "Stand de tir du Bourget",
+        visible: false,
+        tir: true,
+        lebourget: true,
+    }),
+    //Parc des expositions de la porte de Versailles
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Parc des expositions de la porte de Versailles",
+        visible: false,
+        handball: true,
+        tennisdt: true,
+        versailles: true,
+    }),
+    //Parc des Princes
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Parc des Princes",
+        visible: false,
+        football: true,
+        paris: true,
+    }),
+    //Marina olympique du Roucas-Blanc
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Marina olympique du Roucas-Blanc",
+        visible: false,
+        voile: true,
+        marseille: true,
+    }),
+    //Grand Palais
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Grand Palais",
+        visible: false,
+        escrime: true,
+        taekwondo: true,
+        paris: true,
+    }),
+    //Stade Vélodrome
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Stade Vélodrome",
+        visible: false,
+        football: true,
+        marseille: true,
+    }),
+    //Place de la Concorde
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Place de la Concorde",
+        visible: false,
+        basketball: true,
+        breakd: true,
+        escalade: true,
+        skate: true,
+        paris: true,
+    }),
+    //Stade Jean-Bouin
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Stade Jean-Bouin",
+        visible: false,
+        rugby: true,
+        paris: true,
+    }),
+    //Stade Roland-Garros
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Stade Roland-Garros",
+        visible: false,
+        tennis: true,
+        boxe: true,
+        paris: true,
+    }),
+    //Palais omnisports de Paris-Bercy
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Palais omnisports de Paris-Bercy",
+        visible: false,
+        basketball: true,
+        paris: true,
+    }),
+    //Arena Alice Milliat
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Palais omnisports de Paris-Bercy",
+        visible: false,
+        badminton: true,
+        paris: true,
+    }),
+    //Paris La Défense Arena
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Paris La Défense Arena",
+        visible: false,
+        gymnastique: true,
+        paris: true,
+    }),
+    //Teahupo'o
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Teahupo'o",
+        visible: false,
+        surf: true,
+        taiarapu: true,
+    }),
+    //Piscine olympique de Saint-Denis
+    new google.maps.Marker({
+        position: { lat: 4, lng: 2 },
+        map,
+        title: "Piscine olympique de Saint-Denis",
+        visible: false,
+        natation: true,
+        waterpolo: true,
+        stdenis: true,
+    }),
+)
+
+//TABLEAU SPORTS
+const sports = [
+    'Basket-Ball',
+    'Natation',
+    'Athlétisme',
+    'Aviron',
+    'Badminton',
+    'Boxe',
+    'Breakdance',
+    'Canoe-Kayak',
+    'Cyclisme',
+    'Equitation',
+    'Escalade',
+    'Escrime',
+    'Football',
+    'Golf',
+    'Gymnastique',
+    'Halterophilie',
+    'Handball',
+    'Hockey-Gazon',
+    'Judo',
+    'Lutte',
+    'Waterpolo',
+    'Pentathlon moderne',
+    'Rugby',
+    'Skateboard',
+    'Surf',
+    'Taekwondo',
+    'Tennis',
+    'Tennis de table',
+    'Tir',
+    "Tir à l'arc",
+    'Triathlon',
+    'Voile',
+    'Volley-ball',
+]
+
+//TABLEAU VILLES
+const villes = [
+    'Saint-Denis',
+    'Le Bourget',
+    'Paris',
+    'Nanterre',
+    'Colombes',
+    'Vaires-sur-Marne',
+    'Versailles',
+    'Saint-Quentin-en-Yvelines',
+    'Élancourt',
+    'Marseille',
+    'Taiarapu-Ouest(Polynésie française)',
+]
+
+//CUSTOM CONTROL VOIR TOUS LES MARQUERS
+function CenterControl(controlDiv, map) {
+    const controlUI = document.createElement("div");
+    controlUI.style.backgroundColor = "white";
+    controlUI.style.borderRadius = "50px";
+    controlUI.style.boxShadow = "0 5px 6px rgba(0,0,0,.3)";
+    controlUI.style.cursor = "pointer";
+    controlUI.style.marginTop = "10px";
+    controlUI.style.textAlign = "center";
+    controlDiv.appendChild(controlUI);
+    const controlText = document.createElement("div");
+    controlText.style.color = "#003B55";
+    controlText.style.fontFamily = "Roboto,Arial,sans-serif";
+    controlText.style.fontSize = "14px";
+    controlText.style.lineHeight = "30px";
+    controlText.style.paddingLeft = "10px";
+    controlText.style.paddingRight = "10px";
+    controlText.innerHTML = "Recentrer la carte";
+    controlUI.appendChild(controlText);
+    controlUI.addEventListener("click", () => {
+        map.setCenter(mapCenter);
+        map.setZoom(10);
+    });
+}
+
+const centerControlDiv = document.createElement("div");
+CenterControl(centerControlDiv, map);
+map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
+
+//CLICK MARKER
+let currentM = 0;
+for (let i = 0; i < markers.length; i++) {
+    markers[i].addListener('click', function() {
+        $("#fiche-site").removeClass("hidden");
+        $("#site-title").empty().append(this.title);
+        map.setZoom(18);
+        map.setCenter(this.getPosition());
+        currentM = i;
+    });
+    markers[i].visible = true;
+}
+
+$(function() {
+    $('#prev-button').click(function() {
+        if (currentM < 0) {
+            currentM = markers.length;
+        } else
+            currentM -= 1;
+        $("#site-title").empty().append(markers[currentM].title);
+        map.setZoom(18);
+        map.setCenter(markers[currentM].getPosition());
+    });
+    $('#next-button').click(function() {
+        if (currentM > markers.length) {
+            currentM = 0;
+        } else
+            currentM += 1;
+        $("#site-title").empty().append(markers[currentM].title);
+        map.setZoom(18);
+        map.setCenter(markers[currentM].getPosition());
+    });
     $("#quit-button").click(function() {
         $("#fiche-site").addClass("hidden");
         map.setCenter(mapCenter)
         map.setZoom(10);
     });
-
 });
+
+
+
+for (i = 0; i < sports.length; i++) {
+    $("#sport-select").append("<a class='dropdown-item sports-item' href='#'>" + sports[i] + "</a>");
+    $(".sports-item").click(function() {
+        for (i = 0; i < markers.length; i++) {
+            if (markers[i].basketball == true)
+                markers[i].visible = true;
+            else markers[i].visible = false;
+        }
+    })
+}
+
+for (i = 0; i < villes.length; i++) {
+    $("#city-select").append("<a class='dropdown-item' href='#'>" + villes[i] + "</a>");
+}
--- index.php
+++ index.php
@@ -2,17 +2,16 @@
 <html>
 
 <head>
-	<meta charset="utf-8">
-	<title>Olympics Traveler</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/theme.css">
-	
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-		integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title>Olympics Traveler</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/theme.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
-<body class='bg-dark'>
-	
-	<!--Navbar verticale-->
+<body class='bg-jo'>
+
+    <!--Navbar verticale
 		<div class="float-lg-left">
 			<nav id="side-navbar" class="bg-light text-center">
 				<a class="navbar-brand mx-auto" href="#"><?php include 'assets/svg/logoJO2024.php'?></a>
@@ -24,60 +23,59 @@
 					</ul>
 				</div>
 			</nav>
-		</div>
-	<!--Header-->
-	<div class='container text-center'>
-		<?php include 'assets/svg/olympicRings.php'?>
-		<h1 class="text-light position-relative">Olympics' Traveler</h1>
-	</div>
+		</div>-->
+    <!--Header
+    <div class='container text-center'>
+        <h1 class="text-light position-relative mt-3 bg-light text-dark p-2">Olympics' Traveler</h1>
+    </div>-->
 
-	
-		<!--Carte gmaps -->
-		<ul class="nav justify-content-center mb-1">
-        <li class="nav-item">
-            <div class="dropdown">
-                <a class="btn btn-secondary dropdown-toggle mr-2" href="#" role="button" id="dropdownMenuSports" data-toggle="dropdown">Sports</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Basket-Ball</a>
-                    <a class="dropdown-item" href="#">Natation</a>
-                    <a class="dropdown-item" href="#">Athlétisme</a>
+    <!--Carte gmaps -->
+    <div class="container-lg mt-1  ">
+        <ul class="  nav justify-content-center mx-auto nav-map pb-1 pt-2 rounded-top container">
+            <li class="nav-item">
+                <div class="dropdown">
+                    <a class="btn dropdown-toggle mr-2 menu-list text-light" href="#" role="button"
+                        id="dropdownMenuSports" style="background-color: #003B55" data-toggle="dropdown">Sports</a>
+                    <div class="dropdown-menu select-menu overflow-auto" id="sport-select">
+                    </div>
                 </div>
-            </div>
-        </li>
-        <li class="nav-item">
-            <div class="dropdown">
-                <a class="btn btn-secondary dropdown-toggle mr-5" href="#" role="button" id="dropdownMenuSports" data-toggle="dropdown">Villes</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Paris</a>
-                    <a class="dropdown-item" href="#">Versailles</a>
-                    <a class="dropdown-item" href="#">Saint-Denis</a>
+            </li>
+            <li class="nav-item">
+                <div class="dropdown">
+                    <a class="btn dropdown-toggle mr-5 menu-list text-light" href="#" role="button"
+                        id="dropdownMenuSports" style="background-color: #003B55" data-toggle="dropdown">Villes</a>
+                    <div class="dropdown-menu select-menu overflow-auto" id="city-select">
+                    </div>
                 </div>
-            </div>
-        </li>
-        <li>
-            <form class="form-inline">
-                <input class="form-control mr-1" type="search" placeholder="Rechercher">
-                <button class="btn btn-outline-light bg-secondary my-sm-0" type="submit">Rechercher</button>
-            </form>
-        </li>
-    </ul>
-    <div id="map" class="container"></div>
+            </li>
+            <li>
+                <form class="form-inline">
+                    <input class="form-control mr-1" type="search" placeholder="Rechercher">
+                    <button class="btn btn-outline-light my-sm-0" type="submit"
+                        style="background-color: #003B55">Rechercher</button>
+                </form>
+            </li>
+        </ul>
+        <div id="map" class=" container-lg"></div>
 
+    </div>
     <!--Présentation du site-->
-    <div id="fiche-site" class="container mx-auto">
-        <div class="bg-light">
-            <button id="quit-button" class="bg-light float-left">✕</button>
+    <div id="fiche-site" class="container mx-auto hidden mt-3">
+        <div class="bg-light rounded-top" style="border-radius: 20px">
+            <button id="quit-button" class="bg-light float-left rounded-top">✕</button>
             <button id="prev-button" class="bg-light float site-button ">&laquo;</button>
-            <button id="next-button" class="bg-light float-right site-button">&raquo;</button>
+            <button id="next-button" class="bg-light float-right site-button rounded-top">&raquo;</button>
 
         </div>
-        <div class="bg-secondary">
-            <h2 class="bg-light text-center" id="site-title"></h2>
-            <div class="row">
+        <div class="p-2 pt-0 text-center inner-site">
+            <h2 class="bg-light" id="site-title"></h2>
+            <div class="row text-left">
                 <div class="col-sm" id="desc-site">
                     <h4>Description</h4>
                     <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse ipsa corporis suscipit reprehenderit, quaerat ratione architecto sit asperiores ullam dolore expedita culpa minus quasi ipsam officiis? Ab eos in nisi?
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse ipsa corporis suscipit
+                        reprehenderit, quaerat ratione architecto sit asperiores ullam dolore expedita culpa minus quasi
+                        ipsam officiis? Ab eos in nisi?
                     </p>
                 </div>
                 <div id="carouselPhotosSite" class="carousel slide w-50 float-right col-sm" data-ride="carousel">
@@ -102,24 +100,44 @@
                     </a>
                 </div>
             </div>
-            <div class="row">
+            <div class="row text-left">
                 <div class="col-sm">
                     <h4>Epreuves</h4>
                     <p id="epr-site">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magni magnam, alias laborum necessitatibus suscipit perferendis enim hic, nesciunt fugit aperiam blanditiis voluptas architecto labore iusto repudiandae distinctio! Molestias, nulla?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe magni magnam, alias laborum
+                        necessitatibus suscipit perferendis enim hic, nesciunt fugit aperiam blanditiis voluptas
+                        architecto labore iusto repudiandae distinctio! Molestias, nulla?
                     </p>
                 </div>
                 <div class="col-sm">
                     <h4>Lieux à visiter</h4>
                     <p id="visit-site">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae hic eius ab. Accusantium, culpa ducimus eveniet laborum, dolores a fugiat error facere officia praesentium eaque nulla voluptatibus! Voluptates, doloremque ut!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae hic eius ab. Accusantium, culpa
+                        ducimus eveniet laborum, dolores a fugiat error facere officia praesentium eaque nulla
+                        voluptatibus! Voluptates, doloremque ut!
                     </p>
                 </div>
             </div>
         </div>
     </div>
+    <div id="socials-footer" class="container mt-5">
+        <ul class="nav justify-content-center">
+            <li>
+                <a href="https://www.paris2024.org/fr/"><img src="assets/svg/olympicRings.svg" alt="Site Officiel" height="70px"
+                        width="150px" class="nav-item social-icon" ></a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/paris2024/?hl=fr"><img src="assets/svg/instagram.svg" alt="Instagram"
+                        height="70px" width="70px" class="nav-item social-icon ml-4"></a>
+            </li>
+                <a href="https://www.instagram.com/paris2024/?hl=fr"><img src="assets/svg/twitter.svg" alt="Twitter"
+                        height="70px" width="70px" class="nav-item social-icon ml-4"></a>
+            <li>
 
-	<!--Resultats derniere épreuve / Actu ?
+            </li>
+        </ul>
+    </div>
+    <!--Resultats derniere épreuve / Actu ?
 		<section id="result-epr" class="mt-2">
 			<div class="bg-secondary container">
 				<h2 class="bg-light text-center">Résultats de la dernière épreuve</h2>
@@ -138,38 +156,38 @@
 			</div>
 		</section>-->
 
-	<!--Footer-->
-		<footer class="container bg-light mt-5">
-			<div>
-				<p id="footer-descr">
-					Cette application web a été developpé dans le cadre d'un hackathon organisé par l'école DORANCO sur le theme des Jeux Olympiques de 2024.<br>
-					<b>Membres de l'équipe: Didier PRADEL, Adel TERKI, Racim TAMGLIT.</b>
-				</p>
-			</div>
-			<nav>
-				<ul class="nav justify-content-end">
-					<li class="nav-item"><a class="nav-link active" href="#">A propos</a></li>
-					<li class="nav-item"><a class="nav-link" href="#">Nous Contacter</a></li>
-				</ul>
-			</nav>
-		</footer>
+    <!--Footer link vers reseaux sociaux jo
+    <footer class="container bg-light mt-5">
+        <div>
+            <p id="footer-descr">
+                Cette application web a été developpé dans le cadre d'un hackathon organisé par l'école DORANCO sur le
+                theme des Jeux Olympiques de 2024.<br>
+                <b>Membres de l'équipe: Didier PRADEL, Adel TERKI, Racim TAMGLIT.</b>
+            </p>
+        </div>
+        <nav>
+            <ul class="nav justify-content-end">
+                <li class="nav-item"><a class="nav-link active" href="#">A propos</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Nous Contacter</a></li>
+            </ul>
+        </nav>
+    </footer>-->
 
+    <!--SCRIPTS-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+    </script>
+    <script src="http://maps.google.com/maps/api/js?key=AIzaSyAX6pUQMV4uWw7isouqkNfVs7xqrglMjoY&callback=initMap"
+        type="text/javascript"></script>
+    <script src="assets/js/gmaps.js"></script>
+    <script src="assets/js/script.js"></script>
 
-	<!--SCRIPTS-->
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-		integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-	</script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-		integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-	</script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-		integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
-	</script>
-	<script src="http://maps.google.com/maps/api/js?key=AIzaSyAX6pUQMV4uWw7isouqkNfVs7xqrglMjoY&callback=initMap"
-		type="text/javascript"></script>
-	<script src="assets/js/gmaps.js"></script>
-	<script src="assets/js/script.js"></script>
-	<script src="assets/js/carteJO.js"></script>
 </body>
 
 </html>
